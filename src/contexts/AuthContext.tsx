a3e9198b505822decb157de1c1da524ce64e68ea
Olododeveloper <MAC@dadadev.local>
2025-09-25 20:25:18 +0100

    removed lovable icon

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -78,8 +78,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const signInWithGoogle = async () => {
     try {
-      // const redirectUrl = `${window.location.origin}/dashboard`;
-      const redirectUrl = `nexuz.xyz/auth/callback`;
+      const redirectUrl = `${window.location.origin}/dashboard`;
+      // const redirectUrl = `nexuz.xyz/auth/callback`;
 
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
